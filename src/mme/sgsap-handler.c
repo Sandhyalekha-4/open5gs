0db8bc9d889f713e28e9ce0f99fd7708b1bb3c51
Sandhyalekha-4 <sandhyamukkala4@gmail.com>
2025-07-10 16:43:13 +0530

    Update sgsap-handler.c

--- src/mme/sgsap-handler.c
+++ src/mme/sgsap-handler.c
@@ -326,6 +326,8 @@ void sgsap_handle_location_update_reject(mme_vlr_t *vlr, ogs_pkbuf_t *pkbuf)
         ogs_expect(r == OGS_OK);
         if(r!=OGS_OK)
         {
+            ogs_error(" IMSI [%s] and EPS-type [%d] Update location reject",  
+                        mme_ue->imsi_bcd, mme_ue->nas_eps.type);
             ogs_assert(r != OGS_ERROR);
         }
         
