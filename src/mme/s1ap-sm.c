5a35966da208d045087835aabae4a135d5d9fe2a
Sandhyalekha-4 <sandhyamukkala4@gmail.com>
2025-07-17 23:36:54 +0530

    Update s1ap-sm.c

--- src/mme/s1ap-sm.c
+++ src/mme/s1ap-sm.c
@@ -77,8 +77,8 @@ void s1ap_state_operational(ogs_fsm_t *s, mme_event_t *e)
                     S1AP_ProcedureCode_id_S1Setup)) {
             break;
         }
-        ogs_info(" s1ap operational state(choice:%d, proc:%d)",
-                        pdu->present, (int)initiatingMessage->procedureCode);
+       // ogs_info(" s1ap operational state(choice:%d, proc:%d)",
+         //               pdu->present, (int)initiatingMessage->procedureCode);
         switch (pdu->present) {
         case S1AP_S1AP_PDU_PR_initiatingMessage :
             initiatingMessage = pdu->choice.initiatingMessage;
