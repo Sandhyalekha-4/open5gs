a91bfe7db995480324d0ef396230d4c4e465dea9
Sandhyalekha-4 <sandhyamukkala4@gmail.com>
2025-08-04 13:05:24 +0530

    Update pfcp-sm.c

--- src/sgwc/pfcp-sm.c
+++ src/sgwc/pfcp-sm.c
@@ -297,7 +297,10 @@ void sgwc_pfcp_state_associated(ogs_fsm_t *s, sgwc_event_t *e)
 
         case OGS_PFCP_SESSION_MODIFICATION_RESPONSE_TYPE:
             if (!message->h.seid_presence) ogs_error("No SEID");
-
+           if (!sess){
+             ogs_error("sess is NULL, message->h.seid_presence[%d], message h.seid[%" PRIu64 "] and local_seid[%" PRIu64 "]", 
+                 message->h.seid_presence, message->h.seid, xact->local_seid);
+            }
             sgwc_sxa_handle_session_modification_response(
                 sess, xact, e->gtp_message,
                 &message->pfcp_session_modification_response);
@@ -313,10 +316,7 @@ void sgwc_pfcp_state_associated(ogs_fsm_t *s, sgwc_event_t *e)
 
         case OGS_PFCP_SESSION_REPORT_REQUEST_TYPE:
             if (!message->h.seid_presence) ogs_error("No SEID");
-            if (!sess){
-             ogs_error("sess is NULL, message->h.seid_presence[%d], message h.seid[%" PRIu64 "] and local_seid[%" PRIu64 "]", 
-                 message->h.seid_presence, message->h.seid, xact->local_seid);
-            }
+         
             sgwc_sxa_handle_session_report_request(
                 sess, xact, &message->pfcp_session_report_request);
             break;
